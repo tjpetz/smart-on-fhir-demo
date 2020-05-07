67883469313fdcaab71fea6c72940a2b74bc46e4
safurazaheer <38157252+safurazaheer@users.noreply.github.com>
2020-05-07 18:48:40 +0530

    feat(entrypoint): entrypoint.html (#17)

---
+++ static/js/storage.js
@@ -0,0 +1,39 @@
+//driver function
+function storageDriver(obj, storage_type, operation)
+{
+  if(operation == "set")
+  {
+    if(storage_type == "local"){
+      localStore(obj['key'],obj['value']);
+    }
+    else{
+      sessionStore(obj['key'],obj['value']);
+    }
+  }
+  else{
+    if(storage_type == "local"){
+     return  localGet(obj['key']);
+    }
+    else{
+      return sessionGet(obj['key']);
+    }
+  }
+}
+//Function for localstorage
+function localStore(key,value)
+{
+  window.localStorage.setItem(key, value);
+}
+function localGet(key)
+{
+   return window.localStorage.getItem(key);
+}
+//Function for sessionStorage
+function sessionStore(key,value)
+{
+  window.sessionStorage.setItem(key, value);
+}
+function sessionGet(key)
+{
+  return window.sessionStorage.getItem(key);
+}
--- static/js/utils.js
+++ static/js/utils.js
@@ -1,5 +1,5 @@
 // The aim of loadStatus function to check the github page is deployed or not
-// if It is deployed then it will sends the repositry url and time. 
+// if It is deployed then it will sends the repositry url and time.
 function loadStatus() {
   let time = new Date();
   let url = window.location.href;
@@ -8,3 +8,37 @@ function loadStatus() {
 
   document.body.innerHTML = `<pre>` + JSON.stringify(ackJSON) + `</pre>`;
 }
+
+//Function for validating URL
+function isUrlValid(userInput)
+{
+    regexp = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+    if (regexp.test(userInput))
+      return true;
+    else
+      return false;
+ }
+ //Main function
+ function getURL()
+ {
+    //const queryString = "https://something?apiurl=https://api-dev.oninnovaccer.com&oauthurl=https://api-dev.oninnovaccer.com/fhiroauth/oauth2/token&cid=ZPrnAOjtK36ciM2L7Ks67wwhtwCEQUuN";
+    const queryString  =  window.location.href;
+    const url = new URL(queryString);
+    const urlParams = new URLSearchParams(url.search)
+    const api_url = urlParams.get("apiurl");
+    const oauth_url = urlParams.get("oauthurl");
+    const client_id = urlParams.get("cid");
+    let local = "local";let session = "session";
+    let set = "set";let get = "get";
+    if( isUrlValid(api_url) && isUrlValid(oauth_url) )
+    {
+       storageDriver({key: "api_url", value: api_url}, local, set);storageDriver({key: "api_url", value: api_url}, session, set);
+       storageDriver({key: "oauth_url", value: oauth_url}, local, set);storageDriver({key: "oauth_url", value: oauth_url}, session, set);
+       storageDriver({key: "client_id", value: client_id}, local, set);storageDriver({key: "client_id", value: client_id}, session, set);
+       document.getElementById("main").innerHTML = "success";
+    }
+    else if(!isUrlValid(api_url))
+       document.getElementById("main").innerHTML = "Wrong API url";
+    else if(!!isUrlValid(oauth_url))
+       document.getElementById("main").innerHTML = "Wrong Oauth url";
+ }
