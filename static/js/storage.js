8a9b41e1d4af4dd806e68db8b6316f9b2b164a26
RaviRongali <44582859+RaviRongali@users.noreply.github.com>
2020-05-07 18:57:03 +0530

    feat(base): base.html (#16)

--- static/js/storage.js
+++ static/js/storage.js
@@ -1,39 +1,30 @@
 //driver function
-function storageDriver(obj, storage_type, operation)
-{
-  if(operation == "set")
-  {
-    if(storage_type == "local"){
-      localStore(obj['key'],obj['value']);
+function storageDriver(obj, storage_type, operation) {
+  if (operation == "set") {
+    if (storage_type == "local") {
+      localStore(obj["key"], obj["value"]);
+    } else {
+      sessionStore(obj["key"], obj["value"]);
     }
-    else{
-      sessionStore(obj['key'],obj['value']);
-    }
-  }
-  else{
-    if(storage_type == "local"){
-     return  localGet(obj['key']);
-    }
-    else{
-      return sessionGet(obj['key']);
+  } else {
+    if (storage_type == "local") {
+      return localGet(obj["key"]);
+    } else {
+      return sessionGet(obj["key"]);
     }
   }
 }
 //Function for localstorage
-function localStore(key,value)
-{
+function localStore(key, value) {
   window.localStorage.setItem(key, value);
 }
-function localGet(key)
-{
-   return window.localStorage.getItem(key);
+function localGet(key) {
+  return window.localStorage.getItem(key);
 }
 //Function for sessionStorage
-function sessionStore(key,value)
-{
+function sessionStore(key, value) {
   window.sessionStorage.setItem(key, value);
 }
-function sessionGet(key)
-{
+function sessionGet(key) {
   return window.sessionStorage.getItem(key);
 }
